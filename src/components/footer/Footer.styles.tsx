29b19f2b90c9755b5b787ce161c22a6ac6026be4
TuTuanLe <tutuanletkpt@gmail.com>
2022-12-07 23:14:50 +0700

    add footer

--- src/components/footer/Footer.styles.tsx
+++ src/components/footer/Footer.styles.tsx
@@ -1,4 +1,23 @@
 import styled from 'styled-components';
 import { Layout } from 'antd';
 
-export const StyledFooter = styled(Layout.Footer)``;
+export const StyledFooter = styled.section`
+    display: flex;
+    max-width: 100%;
+    margin: 0 120px;
+    gap: 100px;
+    .textTitle {
+        color: #ffffff;
+        font-size: 40px;
+        font-weight: bold;
+        line-height: 60px;
+        margin-bottom: 40px;
+    }
+    .image-bg {
+        flex: 6;
+    }
+    .leftSide {
+        width: 100%;
+        flex: 4;
+    }
+`;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,22 @@
+import { Button } from 'antd';
+import { Link } from 'react-router-dom';
 import { StyledFooter } from './Footer.styles';
 
 export const Footer = () => {
-    return <StyledFooter>78k4/ Cong Hoa Tan Binh</StyledFooter>;
+    return (
+        <StyledFooter>
+            <div className="image-bg">
+                <img src={require('../../assets/images/building.png')} alt="" />
+            </div>
+            <div className="leftSide">
+                <div className="textTitle">
+                    <p>Cong Hoa,</p>
+                    <p>Tan Binh</p>
+                </div>
+                <Link to={'/contact'}>
+                    <Button>Register</Button>
+                </Link>
+            </div>
+        </StyledFooter>
+    );
 };
