4493d1aa36d4a603cc5a2d953817788137b23c5b
TuTuanLe <tutuanletkpt@gmail.com>
2022-12-15 23:10:22 +0700

    update source

--- src/screens/Home/Portfolio/Portfolio.tsx
+++ src/screens/Home/Portfolio/Portfolio.tsx
@@ -17,28 +17,28 @@ const projects = [
         logo: icon1,
         backgroundColor: '#13C4AF',
         backgroundImage: require('../../../assets/images/project1.png'),
-        description: '프리미엄 구독형 전동킥보드 서비스.',
+        description: 'React Native',
     },
     {
         id: 2,
         logo: icon2,
         backgroundColor: '#FFD400',
         backgroundImage: require('../../../assets/images/project2.png'),
-        description: '프리미엄 개인 퀵 택배 서비스.',
+        description: 'React Native',
     },
     {
         id: 3,
         logo: icon3,
         backgroundColor: '#5BD8FD',
         backgroundImage: require('../../../assets/images/project3.png'),
-        description: '온라인 진료 및 생활 건강 안내 서비스.',
+        description: 'React Native.',
     },
     {
         id: 4,
         logo: icon4,
         backgroundColor: '#EBEBEB',
         backgroundImage: require('../../../assets/images/project4.png'),
-        description: '블록체인 기반 슈즈 오너 관리 서비스.',
+        description: 'React Native',
     },
 ];
 
@@ -48,28 +48,28 @@ const projects2 = [
         logo: icon5,
         backgroundColor: '#0059E1',
         backgroundImage: require('../../../assets/images/project5.png'),
-        description: '찾아가는 도심형 렌트카 서비스.',
+        description: 'React Native',
     },
     {
         id: 2,
         logo: icon6,
         backgroundColor: '#3C4489',
         backgroundImage: require('../../../assets/images/project6.png'),
-        description: '결혼 준비 체크리스트 서비스.',
+        description: 'React Native',
     },
     {
         id: 3,
         logo: icon7,
         backgroundColor: '#00BCA0',
         backgroundImage: require('../../../assets/images/project7.png'),
-        description: '목표 관리 소셜 네트워크 앱 서비스.',
+        description: 'React Native',
     },
     {
         id: 4,
         logo: icon8,
         backgroundColor: '#9A6DD8',
         backgroundImage: require('../../../assets/images/project8.png'),
-        description: '우리동네 의료 커뮤니티 서비스.',
+        description: 'React Native',
     },
 ];
 
@@ -77,8 +77,8 @@ export const Portfolio = () => {
     return (
         <StyledPortfolio>
             <div className="title">
-                <span className="header">포트폴리오.</span>
-                <span> 메타업이 함께한 프로젝트입니다.</span>
+                <span className="header">Project.</span>
+                <span> UIT.</span>
             </div>
             <div className="portfolio-row">
                 {projects?.map((item, index) => (
